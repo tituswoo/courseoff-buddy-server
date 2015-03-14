2d46cda34e73b21eeda9e66d9b9675eb32b5f599
Titus Woo <titus.woo@me.com>
2015-03-14 00:14:17 -0400

    finished refactoring all code into modules. can now filter professor info by full, or just averages

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/search/:query', function(req, res) {
  */
 app.get('/course/:id', function(req, res) {
 	res.type('text/javascript');
-	courseCritique.getCourse(req.params.id)
+	courseCritique.getCourseInfo(req.params.id)
 		.then(function(courseInfo) {
 			res.setHeader('Content-Type', 'application/json');
 			res.json(courseInfo);
@@ -62,54 +62,14 @@ app.get('/course/:id', function(req, res) {
  */
 app.get('/prof/:id', function(req, res) {
 	res.type('text/javascript');
-	var url = 'https://critique.gatech.edu/prof.php?id=' + req.params.id;
-	request(url, function (error, response, body) {
-		if (error) throw error;
-		var profInfo = getProfInfo(body, req.params.id);
-		res.setHeader('Content-Type', 'application/json');
-		res.json(profInfo);
-	});
+	courseCritique.getProfessorInfo(req.params.id)
+		.averageMarks().then(function(averages) {
+			console.log(averages);
+			res.setHeader('Content-Type', 'application/json');
+			res.json(averages);
+		});
 });
 
-function getProfInfo(html, profID) {
-	$ = cheerio.load(html);
-
-	var professor = {
-		id: profID,
-		name: $('h2', '.row').text(),
-		averageMarks: {
-			gpa: $('tr', '.row tbody').children().eq(1).text(),
-			a: $('tr', '.row tbody').children().eq(2).text(),
-			b: $('tr', '.row tbody').children().eq(3).text(),
-			c: $('tr', '.row tbody').children().eq(4).text(),
-			d: $('tr', '.row tbody').children().eq(5).text(),
-			f: $('tr', '.row tbody').children().eq(6).text()
-		},
-		courses: []
-	};
-
-	$('tr', '#dataTable tbody').each(function (index, element) {
-		var course = {
-			name: $(this).children().eq(0).text(),
-			id: $(this).children().eq(0).text().replace(/\s/g, ''),
-			section: $(this).children().eq(1).text(),
-			year: $(this).children().eq(2).text(),
-			size: $(this).children().eq(3).text(),
-			averageMarks: {
-				gpa: $(this).children().eq(4).text(),
-				a: $(this).children().eq(5).text(),
-				b: $(this).children().eq(6).text(),
-				c: $(this).children().eq(7).text(),
-				d: $(this).children().eq(8).text(),
-				f: $(this).children().eq(9).text(),
-				w: $(this).children().eq(10).text(),
-			}
-		};
-		professor.courses.push(course);
-	});
-
-	return professor;
-}
 
 var httpServer = http.createServer(app);
 httpServer.listen(3000);
--- courseCritique.js
+++ courseCritique.js
@@ -16,9 +16,9 @@ module.exports = {
 			});
 		});
 	},
-	getCourse: function(cid) {
-		var url = baseURL + 'course.php?id=' + cid;
-		
+	getCourseInfo: function(courseID) {
+		var url = baseURL + 'course.php?id=' + courseID;
+
 		return new Promise(function (resolve, reject) {
 			request(url, function (error, response, body) {
 				if (error) reject(error);
@@ -26,6 +26,39 @@ module.exports = {
 				resolve(courseInfo);
 			});
 		});
+	},
+	getProfessorInfo: function(profID) {
+		var url = baseURL + 'prof.php?id=' + profID;
+
+		var promise = new Promise(function (resolve, reject) {
+			request(url, function (error, response, body) {
+				if (error) reject(error);
+				var profInfo = getProfInfo(body, profID);
+				resolve(profInfo);
+			});
+		});
+
+		return {
+			all: function() {
+				return new Promise(function (resolve, reject) {
+					promise.then(function(profInfo) {
+						resolve(profInfo);
+					}).catch(function(e) {
+						reject(e);
+					});
+				});
+			},
+			averageMarks: function() {
+				return new Promise(function (resolve, reject) {
+					promise.then(function(profInfo) {
+						delete profInfo.courses;
+						resolve(profInfo);
+					}).catch(function(e) {
+						reject(e);
+					});
+				});
+			}
+		};
 	}
 };
 
@@ -79,4 +112,44 @@ function getCourse(html) {
 	});
 
 	return course;
+}
+
+function getProfInfo(html, profID) {
+	$ = cheerio.load(html);
+
+	var professor = {
+		id: profID,
+		name: $('h2', '.row').text(),
+		averageMarks: {
+			gpa: $('tr', '.row tbody').children().eq(1).text(),
+			a: $('tr', '.row tbody').children().eq(2).text(),
+			b: $('tr', '.row tbody').children().eq(3).text(),
+			c: $('tr', '.row tbody').children().eq(4).text(),
+			d: $('tr', '.row tbody').children().eq(5).text(),
+			f: $('tr', '.row tbody').children().eq(6).text()
+		},
+		courses: []
+	};
+
+	$('tr', '#dataTable tbody').each(function (index, element) {
+		var course = {
+			name: $(this).children().eq(0).text(),
+			id: $(this).children().eq(0).text().replace(/\s/g, ''),
+			section: $(this).children().eq(1).text(),
+			year: $(this).children().eq(2).text(),
+			size: $(this).children().eq(3).text(),
+			averageMarks: {
+				gpa: $(this).children().eq(4).text(),
+				a: $(this).children().eq(5).text(),
+				b: $(this).children().eq(6).text(),
+				c: $(this).children().eq(7).text(),
+				d: $(this).children().eq(8).text(),
+				f: $(this).children().eq(9).text(),
+				w: $(this).children().eq(10).text(),
+			}
+		};
+		professor.courses.push(course);
+	});
+
+	return professor;
 }
