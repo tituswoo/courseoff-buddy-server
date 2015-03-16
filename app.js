001f79f08486a1badc4fbd3c2408496ed3a5fc66
Titus Woo <titus.woo@me.com>
2015-03-15 22:28:51 -0400

    Added new CourseBuddy api proxy. Implemented search functionality.

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ var Promise = require('promise');
 var app = express();
 var gatechCatalog = require('./gatechCatalog');
 var courseCritique = require('./courseCritique');
+var courseBuddy = require('./courseBuddy');
+
+courseBuddy.search('CS 4400', 1).then(function (results) {
+	console.log(results);
+}).catch(function (e) {
+	console.log(e);
+});
 
 /**
  * Returns a simple welcome page (root of the API).
@@ -24,16 +31,13 @@ app.get('/', function(req, res) {
 app.get('/search/:query', function(req, res) {
 	res.type('text/javascript');
 
-	var mode = req.query.mode;
-	var searchResults = courseCritique.search(req.params.query);
+	var limit = req.query.limit;
+	var query = req.params.query;
+	var helper = ApiHelper(res);
 
-	var helper = new ApiHelper(res);
-
-	if (mode === 'all') {
-		searchResults.all().then(helper.returnJSON).catch(helper.logErrors);
-	} else if (mode === 'first' || mode === undefined) {
-		searchResults.first().then(helper.returnJSON).catch(helper.logErrors);
-	}
+	courseBuddy.search(query, limit)
+		.then(helper.returnJSON)
+		.catch(helper.logErrors);
 });
 
 /**
--- courseBuddy.js
+++ courseBuddy.js
@@ -1,4 +1,32 @@
 var Promise = require('promise');
+var courseCritique = require('./courseCritique');
+var gatechCatalog = require('./gatechCatalog');
 
-module.exports = {};
+module.exports = new CourseBuddy();
+function CourseBuddy() {};
 
+/**
+ * Search for a class or professor.
+ * @param  {string} query The string query to search for.
+ * @return {[type]}       [description]
+ */
+CourseBuddy.prototype.search = function (query, limit) {
+	return new Promise(function (resolve, reject) {
+		if (query === '') {
+			reject('A query must be specified.');
+		} else {
+			var searchPromise = courseCritique.search(query);
+			limit = limit || -1;
+
+			searchPromise.all().then(function(results) {
+				if (limit > -1) {
+					resolve(results.slice(0, limit));
+				} else {
+					resolve(results);
+				}
+			}).catch(function (error) {
+				reject(error);
+			});
+		}
+	});
+};
